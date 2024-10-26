b750ea362f334bccc29a29107736aeacea56010c
HighMark31 <150486881+HighMark-31@users.noreply.github.com>
2024-10-26 18:56:39 +0200

    Add files via upload

---
+++ effetto MATRIX.bat
@@ -0,0 +1,17 @@
+ecco il codice:
+@echo off
+title Matrix
+color 0a
+mode 1000
+goto Greeting
+
+:Greeting
+cls
+echo Hello Cosimo
+pause
+goto Matrix
+
+
+:Matrix
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto Matrix
